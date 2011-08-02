3d5ff8b279176c2865f7128160bfbf2fd66c7fe6
Marcin Domański <git@kabturek.info>
2011-08-02 20:06:28 +0200

    updated assets template for the new version

--- data/generator/sfDoctrineModule/jqgrid/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/jqgrid/template/templates/_assets.php
@@ -4,5 +4,6 @@
 [?php use_stylesheet('<?php echo sfConfig::get('sf_admin_module_web_dir').'/css/global.css' ?>', 'first') ?] 
 [?php use_stylesheet('<?php echo sfConfig::get('sf_admin_module_web_dir').'/css/default.css' ?>', 'first') ?] 
 <?php endif; ?>
-[?php use_stylesheet('/jqGridAdminTemplatePlugin/js/jqgrid/css/ui.jqgrid.css', 'first') ?] 
-[?php use_javascript("/jqGridAdminTemplatePlugin/js/jqgrid/jquery.jqGrid.js"); ?]
+[?php use_stylesheet("/jqGridAdminTemplatePlugin/css/ui.jqgrid.css", 'first') ?] 
+[?php use_javascript("/jqGridAdminTemplatePlugin/js/i18n/grid.locale-en.js"); ?]
+[?php use_javascript("/jqGridAdminTemplatePlugin/js/jquery.jqGrid.min.js"); ?]
